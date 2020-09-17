2ae6a0e6d73ddb0c66a069ccdb21b6f1c484a9f2
ali raza <aliahmadraza401@gmail.com>
2020-09-17 19:29:54 +0500

    Updates

---
+++ precache-manifest.dea8dd5d18894cb6df955a5638e0de0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73852666217ed40dcf8cdde41084f27",
+    "url": "/Covid-19-App/index.html"
+  },
+  {
+    "revision": "e32a9c88dbcd33e28984",
+    "url": "/Covid-19-App/static/css/main.3c5256de.chunk.css"
+  },
+  {
+    "revision": "5a709bd69febde627ed4",
+    "url": "/Covid-19-App/static/js/2.e31597f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid-19-App/static/js/2.e31597f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32a9c88dbcd33e28984",
+    "url": "/Covid-19-App/static/js/main.eb6d0017.chunk.js"
+  },
+  {
+    "revision": "3102a56cf895fdb65038",
+    "url": "/Covid-19-App/static/js/runtime-main.6161c3f4.js"
+  }
+]);
